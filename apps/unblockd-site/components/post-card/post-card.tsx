5f1d9bf4e1bd3347256bf1feda8a300f922e92ac
beauwilliams <beau.williams@icloud.com>
2022-11-13 11:54:29 +1100

    feat: initial blog page

---
+++ apps/unblockd-site/components/post-card/post-card.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface PostCardProps {}
+
+export function PostCard(props: PostCardProps) {
+  return (
+    <div>
+      <h1>Welcome to PostCard!</h1>
+    </div>
+  );
+}
+
+export default PostCard;
---
+++ apps/unblockd-site/pages/blog/index.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface BlogProps {}
+
+export function Blog(props: BlogProps) {
+  return (
+    <div>
+      <h1>Welcome to Blog!</h1>
+    </div>
+  );
+}
+
+export default Blog;
